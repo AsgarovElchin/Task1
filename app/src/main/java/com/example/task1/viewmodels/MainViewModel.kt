1cef48f41ea91865b7162e0b0ec15b84cb0c163a
ElchinCyrus <71493515+ElchinCyrus@users.noreply.github.com>
2023-04-03 18:54:51 +0400

    MyApplication class added

--- app/src/main/java/com/example/task1/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/task1/viewmodels/MainViewModel.kt
@@ -1,15 +1,41 @@
 package com.example.task1.viewmodels
 
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.task1.data.Repository
 import com.example.task1.models.Products
+import com.example.task1.utils.NetworkResult
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import retrofit2.Response
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val repository: Repository
-) {
+
+)
 
 
-}
+    private fun handleProductsResponse(response: Response<Products>): NetworkResult<Products>? {
+        when {
+            response.message().toString().contains("timeout") -> {
+                return NetworkResult.Error("Timeout")
+            }
+            response.code() == 402 -> {
+                return NetworkResult.Error("API Key Limited.")
+            }
+            response.body()!!.products.isNullOrEmpty() -> {
+                return NetworkResult.Error("Products not found.")
+            }
+            response.isSuccessful -> {
+                val products = response.body()
+                return NetworkResult.Success(products!!)
+            }
+            else -> {
+                return NetworkResult.Error(response.message())
+            }
+        }
+    }
+
+
